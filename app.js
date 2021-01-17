9db1745b38cfad0329965f5ad4f6b53896c860fa
Sadanan Arsaibun <sadananzhang@gmail.com>
2021-01-18 02:49:32 +0700

    delete click bug , complete socket

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const socketio = require("socket.io");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("contrak-1");
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("server is running...");
+});
+
+// Initialize socket for the server
+const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("New user connected");
+
+  socket.username = "Anonymous"
+
+  socket.on("change_username", data => {
+    socket.username = data.username
+  })
+
+  socket.on('interest_add_click', function (data) {
+    io.emit('interest_add_click', data);
+  })
+
+  socket.on('accept_other_add_click', function (data) {
+    io.emit('accept_other_add_click', data);
+  })
+
+  socket.on('Luksub_add_click', function (data) {
+    io.emit('Luksub_add_click', data);
+  })
+
+  socket.on('delete_last_append_click', function (data) {
+    io.emit('delete_last_append_click', data);
+  })
+
+  socket.on('other_add_click', function (data) {
+    io.emit('other_add_click', data);
+  })
+
+  socket.on('reset_click', function (data) {
+    io.emit('reset_click', data);
+  })
+
+});
